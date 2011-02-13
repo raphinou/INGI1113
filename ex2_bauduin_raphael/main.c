3536567d272c868035e09a82cd827646e6d506af
rb <rbauduin@myowndb.com>
2011-02-13 10:28:26 +0100

    pseudo code for password list generation

--- ex2_bauduin_raphael/main.c
+++ ex2_bauduin_raphael/main.c
@@ -1,7 +1,14 @@
 #include "unrar.h"
+#include "pthread.h"
 
 #include <strings.h>
+#include <string.h>
 #include <stdio.h>
+
+struct node{
+	char * password;
+	struct node *next;
+};
 
 static int 
 _unrar_test_password_callback(unsigned int msg, long data, long P1, long P2)
@@ -62,16 +69,74 @@ usage(void)
     printf("rarcrack RAR_FILE [PASSWORD ...]\n");
 }
 
+void
+*test()
+{
+	printf("in test function");
+	pthread_exit(NULL);
+}
+
+node * 
+generate_passwords_list(node * list_head, const char* prefix, alphabet, max_length)
+{
+	int i, alphabet_length = strlen(alphabet);
+	node * head=list_head;
+	char * curent_password;
+	if (strlen(prefix==max_length))
+	{
+		return list_head;
+	}
+	else
+	{
+		/* 2 loops to first add short passwords to the list, then add their children passwords */
+		for(i=0; i< alphabet_length; i++)
+		{
+			current_password = strcat(prefix, alphabet[i]);
+			head=(node *)malloc(sizeof(node));
+			head.password=current_password;
+			head.next = NULL;
+		}
+		for(i=0; i< alphabet_length; i++)
+		{
+			current_password = prefix + alphabet[i];
+			head = generate_passwords_list(head, current_password, alphabet, max_length);
+		}
+	}
+}
+
+void
+generate_passwords(node* list_head, const char *start_letters, const char * alphabet, int max_length)
+{
+/*	int i, j, k, start_letters_length;
+ *	*/
+	int i,j,start_letters_length, alphabet_length;
+	node * head=list_head;
+	printf("start of passwords generation\n");
+	start_letters_length = strlen(start_letters);
+	alphabet_length = strlen(alphabet):
+	printf("start_letter_length = %i\n", start_letters_length);
+	for(i=0; i<start_letters_length;i++)
+	{
+		head = generate_passwords_list(head, head->password, alphabet, max_length);
+	}
+}
+			
 int 
 main (int argc, char const * argv[])
 {
-    int i;
-    
+    int i, thread_status;
+	pthread_t threads[2];
+/*    
     if (argc < 2) {
         usage();
         return 1;
     }
-    
+*/    
+	char  s1[]="abc",s2[]="def",alphabet[]="abcdef";
+	generate_passwords(s1, alphabet, 2);
+	
+	printf("%s,%s,%s", s1,s2,alphabet);
+	thread_status = pthread_create(&threads[0], NULL, test, NULL);
     for (i = 2; i <= argc; i++) {
         if(unrar_test_password(argv[1], argv[i]) == 0) {
             printf("Password is: %s\n", argv[i]);
