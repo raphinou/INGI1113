31ad7d621be8cd8c327184d75b1cc282d23b81ad
Raphael Bauduin <rb@raphinou.com>
2011-02-19 12:33:01 +0100

    iterates overs dictionary

--- ex3_bauduin_raphael/main.c
+++ ex3_bauduin_raphael/main.c
@@ -9,11 +9,45 @@ usage (void)
     printf("zipcrack ZIP_FILE [PASSWORD1 [PASSWORD2 ...]]\n");
 }
 
+int
+read_word(FILE * fp, char * password ) 
+/* reads one line from the file, which is put in password 
+ * returns 1 for successful operation, 0 otherwise (at EOF) */
+{
+	char c;
+	while(((c=getc(fp))!='\n'))
+	{
+		if (c==EOF)
+		{
+			return 0;
+		}
+		*(password++)=c;
+		/* this is necessary to have \0 at the end of the last word of the file when it is read */
+		*password='\0';
+	}
+	return 1;
+}
+void
+read_dico(char * file) 
+/* opens file path passed in parameter, and reads it line per line 
+ * each password should be maximum 29 characters long */
+{
+	FILE * fp;
+	int counter=0;
+	char password[30]="";
+	fp=fopen(file, "r");
+	while (counter++<20 && read_word(fp, password))
+		printf("PASSSWORD READ is %s\n", password);
+
+}
+
 int 
 main (int argc, char const * argv[])
 {
     int i;
     struct zip_archive * archive;
+
+	read_dico("dico.txt");
 
     if (argc < 2) {
         usage();
